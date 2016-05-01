f7197dcf9fba2a926e6cfd8cb3bc8c96aea8c9db
Shusui MOYATANI <syusui.s@gmail.com>
2016-05-01 22:47:20 +0900

    ES6バンバン使って非同期処理を綺麗にした

--- pixiv_bookmark_modal.user.js
+++ pixiv_bookmark_modal.user.js
@@ -6,14 +6,46 @@
 // @version           0.0.1
 // ==/UserScript==
 
+'use strict';
+
 var modalContainerId = 'bookark-modal-container';
 
-function getIllustPage(id) {
-	if (! /^\d+$/.test(id)) { return null; }
+function async(gene) {
+	const result = gene.next();
 
+	if (result.done) return;
+
+	if (result.value instanceof Promise) {
+		result.value.then(() => {
+			async(gene);
+		}, err => console.log(err) );
+	} else {
+		async(gene);
+	}
+}
+
+function polling(func, times, timeout) {
+	if (!(	func instanceof Function &&
+			typeof times == "number" &&
+			typeof timeout == "number"
+		) ) {
+		return Promise.reject();
+	}
+
+	return new Promise((resolve, reject) => {
+		let count = 0;
+
+		let id = window.setInterval(() => {
+			if ( func() ) { resolve(); }
+			else if (count < times) { count++; }
+			else { window.clearInterval(id); reject(); }
+		}, timeout);
+	});
+}
+
+function getDocument(url) {
 	const promise = new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
-		const url = `http://www.pixiv.net/member_illust.php?mode=medium&illust_id=${id}`;
 
 		xhr.addEventListener('load', (e) => {
 			const xhr = e.target;
@@ -35,56 +67,98 @@ function getIllustPage(id) {
 	return promise;
 }
 
+function getBookmarkEditPage(id) {
+	if (! /^\d+$/.test(id)) { return null; }
+	
+	const url = `http://www.pixiv.net/bookmark_add.php?type=illust&illust_id=${id}`;
+	return getDocument(url);
+}
+
+function getIllustPage(id) {
+	if (! /^\d+$/.test(id)) { return null; }
+
+	const url = `http://www.pixiv.net/member_illust.php?mode=medium&illust_id=${id}`;
+	return getDocument(url);
+}
+
+function setBookmarkTags(id) {
+	const input_tag = document.querySelector('#input_tag');
+
+	return new Promise((resolve, reject) => {
+		getBookmarkEditPage(id).then(
+			content => {
+				const doc = document.createElement('html');
+				doc.innerHTML = content;
+
+				const bookmark_tag = doc.querySelector('#input_tag');
+				const tags = bookmark_tag.value;
+				if (tags.length > 0) {
+					input_tag.value = tags;
+				}
+				resolve();
+			},
+			error => {
+				window.alert('failed to fetch bookmark page');
+				reject();
+			}
+		);
+	});
+}
+
 function removeBookmarkModalContainer() {
 	const currentModalElem =
 		document.querySelector(`div#${modalContainerId}`);
 	if (currentModalElem) { currentModalElem.remove(); }
 }
 
-function showBookmarkModal(id) {
-	getIllustPage(id).then(
-		content => {
-			const doc = document.createElement('html');
-			doc.innerHTML = content;
+function *showBookmarkModal(id) {
+	yield new Promise((resolve, reject) => {
+		getIllustPage(id).then(
+			content => {
+				const doc = document.createElement('html');
+				doc.innerHTML = content;
 
-			const modalElem = doc.querySelector('#wrapper > div.bookmark-add-modal');
-			const tagsContainer = doc.querySelector('span.tags-container');
+				const modalElem = doc.querySelector('#wrapper > div.bookmark-add-modal');
+				const tagsContainer = doc.querySelector('span.tags-container');
 
-			const modalContainer = document.createElement('div');
-			modalContainer.id = modalContainerId;
-			modalContainer.appendChild(modalElem);
-			modalContainer.appendChild(tagsContainer);
+				const modalContainer = document.createElement('div');
+				modalContainer.id = modalContainerId;
+				modalContainer.appendChild(modalElem);
+				modalContainer.appendChild(tagsContainer);
 
-			removeBookmarkModalContainer();
-			document.body.appendChild(modalContainer);
+				removeBookmarkModalContainer();
+				document.body.appendChild(modalContainer);
 
-			location.assign('javascript: pixiv.bookmarkModal.initialize();');
+				// show bookmark modal
+				location.assign('javascript: pixiv.bookmarkModal.initialize();');
 
-			window.setTimeout(() => {
-				window.postMessage('pixiv_auto_tag:generateButtons', location.origin);
-				window.postMessage('pixiv_auto_tag:autoTag', location.origin);
-			}, 700);
-		},
-		error => {
-			window.alert('failed to fetch illust page');
-		}
-	);
+				resolve();
+			},
+			error => {
+				reject('failed to fetch illust page');
+			}
+		);
+	});
+
+	yield polling(() => document.querySelector('#input_tag'), 200, 50);
+	
+	yield setBookmarkTags(id);
+
+	window.postMessage('pixiv_auto_tag:generateButtons', location.origin);
+	window.postMessage('pixiv_auto_tag:autoTag', location.origin);
 }
 
 (function () {
 	const query = '#f > div.display_editable_works > ul > li > a.edit-work';
 	const editLinks = document.querySelectorAll(query);
 
-	const editBookmark = (ev) => {
-		const id = ev.target.href.match(/_id=(\d+)/)[1];
-		showBookmarkModal(id);
-		ev.preventDefault();
-	};
+	Array.prototype.forEach.call(editLinks, (link) => {
+		const id = link.href.match(/_id=(\d+)/)[1];
 
-	const setListener = (link) => {
-		link.addEventListener('click', editBookmark);
+		link.addEventListener('click', (ev) => {
+			async(showBookmarkModal(id));
+			ev.preventDefault();
+		});
 		link.href = 'javascript:void(0);';
-	};
-
-	Array.prototype.forEach.call(editLinks, setListener);
+	});
 })();
