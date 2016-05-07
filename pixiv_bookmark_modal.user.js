185d0c01e49583f5b6ec32f88c64b970788922f2
Shusui MOYATANI <syusui.s@gmail.com>
2016-05-07 15:13:26 +0900

    いい感じにした

--- pixiv_bookmark_modal.user.js
+++ pixiv_bookmark_modal.user.js
@@ -8,42 +8,137 @@
 
 'use strict';
 
-var modalContainerId = 'bookark-modal-container';
+const modalContainerId = 'bookark-modal-container';
 
-function async(gene) {
-	const result = gene.next();
+const overlay = document.createElement('div');
+overlay.style.position  = 'fixed';
+overlay.style.top       = '0';
+overlay.style.left      = '0';
+overlay.style.height    = '200%';
+overlay.style.width     = '100%';
+overlay.style.zIndex    = '99999';
+overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+overlay.style.backgroundAttachment = 'fixed';
+overlay.style.backgroundRepeat = 'no-repeat';
+overlay.style.backgroundPosition = 'center center';
+overlay.style.paddingTop = '10px';
+overlay.style.textAlign = 'center';
+overlay.style.fontSize = '20px';
+overlay.style.fontWeight = '900';
+overlay.style.color = 'white';
+
+function async(gene, previous_result) {
+	const result = gene.next(previous_result);
 
 	if (result.done) return;
 
-	if (result.value instanceof Promise) {
-		result.value.then(() => {
-			async(gene);
-		}, err => console.log(err) );
+	const value = result.value;
+
+	if (value instanceof Promise) {
+		value.then((promise_result) => {
+			async(gene, promise_result);
+		}, (err) => {
+			console.log(err);
+		});
+
+		value.catch((err) => {
+			console.log(err);
+		});
 	} else {
-		async(gene);
+		async(gene, value);
 	}
 }
 
-function polling(func, times, timeout) {
-	if (!(	func instanceof Function &&
-			typeof times == "number" &&
-			typeof timeout == "number"
+function polling(func, polling_time, times, timeout) {
+	if (! ( func instanceof Function &&
+			typeof polling_time === "number" &&
+			typeof times === "number" &&
+			typeof timeout === "number"
 		) ) {
-		return Promise.reject();
+		return Promise.reject('polling called with invalid argument(s)');
 	}
 
 	return new Promise((resolve, reject) => {
 		let count = 0;
+		let done  = false;
 
 		let id = window.setInterval(() => {
-			if ( func() ) { resolve(); }
-			else if (count < times) { count++; }
-			else { window.clearInterval(id); reject(); }
+			if ( func() ) {
+				done = true;
+				window.clearInterval(id);
+				resolve();
+			} else if (count < times) {
+				count++;
+			} else {
+				window.clearInterval(id);
+				reject('polling reach a maximum retry count');
+			}
+		}, polling_time);
+
+		window.setTimeout(() => {
+			if (!done) {
+				window.clearInterval(id);
+				reject('polling timeout');
+			}
 		}, timeout);
 	});
 }
 
-function getDocument(url) {
+function sleep(timeout) {
+	if (! typeof timeout === "number" ) {
+		return Promise.reject('sleep called with an invalid argument');
+	}
+
+	return new Promise((resolve, reject) => {
+		window.setTimeout(() => resolve(), timeout);
+	});
+}
+
+function formToObj(node) {
+	if (! (node instanceof HTMLFormElement)) {
+		return null;
+	}
+
+	const result = {};
+
+	const inputs = node.querySelectorAll('input');
+	Array.prototype.forEach.call(inputs, (input) => {
+		const type = input.value;
+
+		if (/^radio$/i.test(type) && input.checked) {
+			return;
+		} else if (/^button|submit|reset|image$/.test(type)) {
+			return;
+		}
+
+		result[input.name] = input.value;
+	});
+
+	const selects = node.querySelectorAll('select');
+	Array.prototype.forEach.call(selects, (select) => {
+		const options = select.querySelectorAll('option');
+
+		Array.prototype.filter.call(
+			options,
+			(e) => e.selected
+		).forEach((opt) => {
+			result[select.name] = opt.value;
+		});
+	});
+
+	return result;
+}
+
+function serializeForm(obj) {
+	let result = Array.prototype.map.call(
+		Object.keys(obj),
+		(key) => `${key}=${obj[key]}`
+	).join('&');
+	
+	return encodeURI(result);
+}
+
+function promiseXHR(method, url, form) {
 	const promise = new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
 
@@ -61,10 +156,26 @@ function getDocument(url) {
 			reject(xhr.statusText);
 		});
 
-		xhr.open('GET', url);
-		xhr.send();
+		xhr.open(method, url, true);
+
+		
+		if (form) {
+			xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			xhr.send( serializeForm(form) );
+		} else {
+			xhr.send();
+		}
 	});
 	return promise;
+
+}
+
+function getDocument(url) {
+	return promiseXHR('GET', url);
+}
+
+function postForm(url, form) {
+	return promiseXHR('POST', url, form);
 }
 
 function getBookmarkEditPage(id) {
@@ -81,28 +192,50 @@ function getIllustPage(id) {
 	return getDocument(url);
 }
 
-function setBookmarkTags(id) {
-	const input_tag = document.querySelector('#input_tag');
+function preventScroll(ev) {
+	window.scrollTo(window.pageXOffset, window.pageYOffset);
+}
 
-	return new Promise((resolve, reject) => {
-		getBookmarkEditPage(id).then(
-			content => {
-				const doc = document.createElement('html');
-				doc.innerHTML = content;
+function onBookmarkSubmit(ev) {
+	const form = ev.target;
 
-				const bookmark_tag = doc.querySelector('#input_tag');
-				const tags = bookmark_tag.value;
-				if (tags.length > 0) {
-					input_tag.value = tags;
-				}
-				resolve();
-			},
-			error => {
-				window.alert('failed to fetch bookmark page');
-				reject();
+	async( (function *() {
+		window.addEventListener('scroll', preventScroll);
+		overlay.textContent = 'ブックマークしています...';
+		document.body.appendChild(overlay);
+
+		const formObj = formToObj(form);
+		yield postForm(form.action, formObj);
+
+		overlay.textContent = '完了しました!';
+		yield sleep(500);
+		document.body.removeChild(overlay);
+		window.removeEventListener('scroll', preventScroll);
+
+		// remove a work if its tag doesn't have a bookmark page tag;
+		const qstr_match = location.href.match(/[?&]tag=([^&#]*)/);
+		const input_tag = document.querySelector('#input_tag');
+
+		if (qstr_match &&
+			input_tag.value.replace(/\s+/g, '').length !== 0 &&
+			! input_tag.value.includes(decodeURI(qstr_match[1]))
+		) {
+			const id = formObj['id'];
+			const works = document.querySelector('ul._image-items');
+			const links = works.querySelectorAll('li.image-item > a.work');
+			const found = Array.prototype.find.call(links, (link) => {
+				return link.href.includes(id);
+			});
+
+			if (found) {
+				works.removeChild(found.parentNode);
 			}
-		);
-	});
+		}
+
+		document.querySelector("#bookark-modal-container > div > div").click();
+	})() );
+
+	ev.preventDefault();
 }
 
 function removeBookmarkModalContainer() {
@@ -111,41 +244,50 @@ function removeBookmarkModalContainer() {
 	if (currentModalElem) { currentModalElem.remove(); }
 }
 
+function* setBookmarkTags(id) {
+	const input_tag = document.querySelector('#input_tag');
+
+	const content = yield getBookmarkEditPage(id);
+	const doc = document.createElement('html');
+	doc.innerHTML = content;
+
+	const bookmark_tag = doc.querySelector('#input_tag');
+	const tags = bookmark_tag.value;
+	if (tags.length > 0) {
+		input_tag.value = tags;
+	}
+}
+
 function *showBookmarkModal(id) {
-	yield new Promise((resolve, reject) => {
-		getIllustPage(id).then(
-			content => {
-				const doc = document.createElement('html');
-				doc.innerHTML = content;
+	// fetch illust page to show modal
+	const content = yield getIllustPage(id);
+	const doc = document.createElement('html');
+	doc.innerHTML = content;
 
-				const modalElem = doc.querySelector('#wrapper > div.bookmark-add-modal');
-				const tagsContainer = doc.querySelector('span.tags-container');
+	const modalElem = doc.querySelector('#wrapper > div.bookmark-add-modal');
+	const tagsContainer = doc.querySelector('span.tags-container');
 
-				const modalContainer = document.createElement('div');
-				modalContainer.id = modalContainerId;
-				modalContainer.appendChild(modalElem);
-				modalContainer.appendChild(tagsContainer);
+	// append modal elements
+	const modalContainer = document.createElement('div');
+	modalContainer.id = modalContainerId;
+	modalContainer.appendChild(modalElem);
+	modalContainer.appendChild(tagsContainer);
+	modalElem.querySelector('form').addEventListener('submit', onBookmarkSubmit);
 
-				removeBookmarkModalContainer();
-				document.body.appendChild(modalContainer);
+	removeBookmarkModalContainer();
+	document.body.appendChild(modalContainer);
 
-				// show bookmark modal
-				location.assign('javascript: pixiv.bookmarkModal.initialize();');
+	// show bookmark modal
+	location.assign('javascript: pixiv.bookmarkModal.initialize();');
 
-				resolve();
-			},
-			error => {
-				reject('failed to fetch illust page');
-			}
-		);
-	});
+	// wait for generation of #input_tag
+	yield polling(() => document.querySelector('#input_tag'), 50, 200, 50*200); // 50ms * 200times
 
-	yield polling(() => document.querySelector('#input_tag'), 200, 50);
-	
-	yield setBookmarkTags(id);
+	yield* setBookmarkTags(id);
 
-	window.postMessage('pixiv_auto_tag:generateButtons', location.origin);
-	window.postMessage('pixiv_auto_tag:autoTag', location.origin);
+	// PixivAutoTag.user.js
+	window.dispatchEvent(new CustomEvent('PixivAutoTag.generateButtons'));
+	window.dispatchEvent(new CustomEvent('PixivAutoTag.autoTag'));
 }
 
 function rewriteEditLinks() {
